7f94f46ba4ea65e9516a08ee750c0dfc4bd0dec5
Jessi <7565683-Jessi777@users.noreply.gitlab.com>
2022-11-18 22:34:42 -0800

    take off

---
+++ docs/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.bootstrap = void 0;
+const bootstrap = (input_number) => console.log(`app started ${input_number}`);
+exports.bootstrap = bootstrap;
